1def9cfb8005661c339c774447b24b4c6f39b139
Nikita Farisey <niksfarisey@gmail.com>
2026-05-21 23:17:02 +0200

    Initial Commit

---
+++ BookMaster/BookMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMasterApp.swift
+//  BookMaster
+//
+//  Created by Nikita on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
